533fcd35d8627dea7ea5c8a6a0fc2a33624accff
natmark <natmark0918@gmail.com>
2017-04-17 11:13:06 +0900

    :new: Add project file

---
+++ NALiveSketchView/NALiveSketchView.h
@@ -0,0 +1,19 @@
+//
+//  NALiveSketchView.h
+//  NALiveSketchView
+//
+//  Created by AtsuyaSato on 2017/04/17.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NALiveSketchView.
+FOUNDATION_EXPORT double NALiveSketchViewVersionNumber;
+
+//! Project version string for NALiveSketchView.
+FOUNDATION_EXPORT const unsigned char NALiveSketchViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NALiveSketchView/PublicHeader.h>
+
+
